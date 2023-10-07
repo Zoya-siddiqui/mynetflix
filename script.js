85746f678b36e9359ba99337ed6aff8a7b282547
zoya-siddiqui <zouas3423@gmail.com>
2023-10-07 14:07:55 +0530

    hi

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 var acc = document.getElementsByClassName("accordion");
 let btnLogin=document.getElementById("myBtn")
 let inputLoginemail=document.getElementsByClassName("ea")
-// var i;
+var i;
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.maxHeight) {
-//       panel.style.maxHeight = null;
-//     } else {
-//       panel.style.maxHeight = panel.scrollHeight + "px";
-//     } 
-//   });
-// }
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
 
 // //-------------------------
 // let subscriber1={
